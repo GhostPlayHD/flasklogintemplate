15519f99ad45a7c74dbe60681255e7ea81a8028a
GhostPlay <99086861+GhostPlayHD@users.noreply.github.com>
2024-09-13 19:44:07 +0000

    updated and edited base + index

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        hashed_pw = generate_password_hash(form.password.data, method="sha256")
+        hashed_pw = generate_password_hash(form.password.data, method="pbkdf2")
         new_user = Users(username=form.username.data, email=form.email.data, password=hashed_pw)
         db.session.add(new_user)
         db.session.commit()
@@ -82,5 +82,6 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
